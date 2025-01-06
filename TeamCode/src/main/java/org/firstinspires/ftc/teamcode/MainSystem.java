6f2c6165ae02e92d83986c09ead92fc906015f93
SantiQ <a01571222@itesm.mx>
2025-01-06 00:54:18 -0600

    Post-vacation update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
@@ -30,82 +30,76 @@ import org.firstinspires.ftc.teamcode.Commands.Drive;
 @TeleOp
 public class MainSystem extends LinearOpMode {
     @Override
-        public void runOpMode(){
+    public void runOpMode(){
           
-            CommandScheduler.getInstance().cancelAll();
-            CommandScheduler.getInstance().reset();
-            telemetry = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry());
+    CommandScheduler.getInstance().cancelAll();
+    CommandScheduler.getInstance().reset();
+    telemetry = new MultipleTelemetry(FtcDashboard.getInstance().getTelemetry());
 
-            Chassis chassis = new Chassis(hardwareMap); //Here you can add every element of the robot
-            Intake intake = new Intake(hardwareMap);
-            Arm arm = new Arm(hardwareMap);
-            GamepadEx driver = new GamepadEx(gamepad1);
-            GamepadEx operator = new GamepadEx(gamepad2);
-            Elevator elevator = new Elevator(hardwareMap);
+    /* SUBSYSTEM DECLARATION */
+    Chassis chassis = new Chassis(hardwareMap);
+    Intake intake = new Intake(hardwareMap);
+    Arm arm = new Arm(hardwareMap);
+    Elevator elevator = new Elevator(hardwareMap);
 
-        /*------------------------------------------------------------------*/
-        /*------------------------------------------------------------------*/
+    /* GAMEPAD DECLARATION */
+    GamepadEx driver = new GamepadEx(gamepad1);
+    GamepadEx operator = new GamepadEx(gamepad2);
 
-            chassis.setDefaultCommand(new Drive(chassis,gamepad1));
+    /* COMMAND DECLARATION */
+        // CHASSIS
+        chassis.setDefaultCommand(new Drive(chassis,gamepad1));
 
-            Button operatorButtonX= operator.getGamepadButton(GamepadKeys.Button.X);
-            operatorButtonX.whenHeld(new MoveIntake(intake,1.0));
-            operatorButtonX.whenReleased(new MoveIntake(intake,0.0));
+        // INTAKE
+        Button driverButtonX= driver.getGamepadButton(GamepadKeys.Button.X);
+        driverButtonX.whenHeld(new MoveIntake(intake,1.0));
+        driverButtonX.whenReleased(new MoveIntake(intake,0.0));
 
-            Button operatorButtonB= operator.getGamepadButton(GamepadKeys.Button.B);
-            operatorButtonB.whenHeld(new MoveIntake(intake,-1.0));
-            operatorButtonB.whenReleased(new MoveIntake(intake,0.0));
+        Button driverButtonB= driver.getGamepadButton(GamepadKeys.Button.B);
+        driverButtonB.whenHeld(new MoveIntake(intake,-1.0));
+        driverButtonB.whenReleased(new MoveIntake(intake,0.0));
 
+        // ARM
         Button operatorButtonA= operator.getGamepadButton(GamepadKeys.Button.A);
         operatorButtonA.whenPressed(new MoveArm(arm,90.0));
 
         Button operatorButtonY= operator.getGamepadButton(GamepadKeys.Button.Y);
         operatorButtonY.whenPressed(new MoveArm(arm,0.0));
 
-        //Button operatorButtonY= operator.getGamepadButton(GamepadKeys.Button.Y);
-        //operatorButtonY.whenPressed(new ElevatorPositions(elevator,60.0));
-
-        Button operatorButtonDPAD= operator.getGamepadButton(GamepadKeys.Button.DPAD_UP);
-        operatorButtonDPAD.whenPressed(new ElevatorPositions(elevator,60.0));
-
-        Button operatorButtonDPD= operator.getGamepadButton(GamepadKeys.Button.DPAD_DOWN);
-        operatorButtonDPD.whenPressed(new ElevatorPositions(elevator,0.0));
-
-        /*Button operatorButtonDPAD= operator.getGamepadButton(GamepadKeys.Button.DPAD_UP);
-        operatorButtonDPAD.whenPressed(new LowBasket(elevator arm));*/
-
-
+        // ELEVATOR
         Button operatorButtonX= operator.getGamepadButton(GamepadKeys.Button.X);
-        operatorButtonX.whenPressed(new ElevatorPositions(elevator,0));
+        operatorButtonX.whenPressed(new ElevatorPositions(elevator,60.0));
 
+        Button operatorButtonDPADUP= operator.getGamepadButton(GamepadKeys.Button.DPAD_UP);
+        operatorButtonDPADUP.whenPressed(new ElevatorPositions(elevator,60.0));
 
-        Button operatorButtonA=operator.getGamepadButton(GamepadKeys.Button.A);
-        operatorButtonA.whenPressed(new BasketPos(arm, elevator, Constants.Arm.ARMHIGHCHAMBER, Constants.Elevator.ELEVATORHIGHCHAMBER));
+        Button operatorButtonDPADDOWN= operator.getGamepadButton(GamepadKeys.Button.DPAD_DOWN);
+        operatorButtonDPADDOWN.whenPressed(new ElevatorPositions(elevator,0.0));
 
+        // GAME ROUTINES
+        //Button operatorButtonA=operator.getGamepadButton(GamepadKeys.Button.A);
+        //operatorButtonA.whenPressed(new BasketPos(arm, elevator, Constants.Arm.ARMHIGHCHAMBER, Constants.Elevator.ELEVATORHIGHCHAMBER));
+
+        //Button operatorButtonDPAD= operator.getGamepadButton(GamepadKeys.Button.DPAD_UP);
+        //operatorButtonDPAD.whenPressed(new LowBasket(elevator arm));
 
         waitForStart();
-            chassis.reset(new Pose2d(0,0, Rotation2d.fromDegrees(0))); /*When the Op mode starts,
-                                                                every value will return to be zero*/
+            chassis.reset(new Pose2d(0,0, Rotation2d.fromDegrees(0)));
 
-
-
-            while (opModeIsActive()) { //This will occur whenever the op mode is active
-
-
+            while (opModeIsActive()) {
                 CommandScheduler.getInstance().run();
                 Pose2d pose = chassis.getPose();
 
                 // -- ODOMETRY TELEMETRY -- //
-                /*telemetry.addData("X", pose.getX()); //This will display the telemetry on the DriverHub
-                telemetry.addData("Y", pose.getY());
-                telemetry.addData("Heading", pose.getRotation().getDegrees());
-                telemetry.addData("RightDistance", chassis.rightDistance());
-                telemetry.addData("LeftDistance", chassis.leftDistance());
-                telemetry.addData("Potentiometer voltage", arm.getVoltage());*/
-                telemetry.addData("Elevator_Distance", elevator.getHeight());
+                    telemetry.addData("X", pose.getX());
+                    telemetry.addData("Y", pose.getY());
+                    telemetry.addData("Heading", pose.getRotation().getDegrees());
+                    telemetry.addData("RightDistance", chassis.rightDistance());
+                    telemetry.addData("LeftDistance", chassis.leftDistance());
+                    telemetry.addData("Elevator_Distance", elevator.getHeight());
 
                 // -- UPDATE TELEMETRY -- //
-                telemetry.update();
+                    telemetry.update();
 
             }
     }
