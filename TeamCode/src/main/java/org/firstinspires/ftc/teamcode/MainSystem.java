23794eeb408e2e5a72359fff4ca0d0979593a548
GabrielReynaFranco <a01572655@tec.mx>
2025-01-08 20:24:33 -0600

    Tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainSystem.java
@@ -130,11 +130,6 @@ public class MainSystem extends LinearOpMode {
                 CommandScheduler.getInstance().run();
                 Pose2d pose = chassis.getPose();
 
-                if (arm.isTouchPressed()){
-                    telemetry.addData("Reset Encoder", "Reset");
-                } else {
-                    telemetry.addData("Reset Encoder", "Not Reset");
-                }
 
                 // -- ODOMETRY TELEMETRY -- //
                     telemetry.addLine("--- IMU Telemetry ---");
