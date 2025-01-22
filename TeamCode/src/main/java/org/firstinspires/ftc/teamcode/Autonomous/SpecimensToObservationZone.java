e7df83a075de203ccdf6c463698b3b3d3c8827f6
Emilio <eramirezc145@gmail.com>
2025-01-22 17:57:49 -0600

    modified the 2 high basket and park autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SpecimensToObservationZone.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SpecimensToObservationZone.java
@@ -46,76 +46,58 @@ public class ThreeSpecimensToObservationZone extends LinearOpMode {
         TrajectoryConfig forwardConfig = new TrajectoryConfig(0.5, 0.2);
         forwardConfig.setReversed(false);
         Trajectory FirstSample = TrajectoryGenerator.generateTrajectory(Arrays.asList(
-                new Pose2d(0,0,Rotation2d.fromDegrees(-50 /*Pending Test*/ )),
-                new Pose2d(-1,-1 /*All Distances to be measured*/ ,Rotation2d.fromDegrees(-50 /*Pending Test*/ ))), forwardConfig
+                new Pose2d(0,0,Rotation2d.fromDegrees(0 )),
+                new Pose2d(1.45,-0.85 ,Rotation2d.fromDegrees(0))), forwardConfig
         );
 
         TrajectoryConfig forwardConfig2 = new TrajectoryConfig(0.5, 0.2);
         forwardConfig.setReversed(false);
         Trajectory GoToObservationZone1 = TrajectoryGenerator.generateTrajectory(Arrays.asList(
-                new Pose2d(-1,-1,Rotation2d.fromDegrees(-130 /*Pending Test*/ )),
-                new Pose2d(0,-1 ,Rotation2d.fromDegrees(-130 /*Pending Test*/ ))), forwardConfig2
+                new Pose2d(1.45,-0.85,Rotation2d.fromDegrees(0)),
+                new Pose2d(0,-1 ,Rotation2d.fromDegrees(0))), forwardConfig2
         );
 
         TrajectoryConfig forwardConfig3 = new TrajectoryConfig(0.5, 0.2);
         forwardConfig.setReversed(false);
         Trajectory SecondSample = TrajectoryGenerator.generateTrajectory(Arrays.asList(
-                new Pose2d(0,-1,Rotation2d.fromDegrees(-55 /*Pending Test*/ )),
-                new Pose2d(-2,-2 ,Rotation2d.fromDegrees(-55 /*Pending Test*/ ))), forwardConfig3
+                new Pose2d(0,-1,Rotation2d.fromDegrees(0)),
+                new Pose2d(1.45,-1.07 ,Rotation2d.fromDegrees(0))), forwardConfig3
         );
 
         TrajectoryConfig forwardConfig4 = new TrajectoryConfig(0.5, 0.2);
         forwardConfig.setReversed(false);
         Trajectory GoToObservationZone2 = TrajectoryGenerator.generateTrajectory(Arrays.asList(
-                new Pose2d(-2,-2,Rotation2d.fromDegrees(-130 /*Pending Test*/ )),
-                new Pose2d(0,-1 ,Rotation2d.fromDegrees(-130 /*Pending Test*/ ))), forwardConfig4
+                new Pose2d(1.45,-1.07,Rotation2d.fromDegrees(0)),
+                new Pose2d(0,-1 ,Rotation2d.fromDegrees(0))), forwardConfig4
         );
 
         TrajectoryConfig forwardConfig5 = new TrajectoryConfig(0.5, 0.2);
         forwardConfig.setReversed(false);
         Trajectory ThirdSample = TrajectoryGenerator.generateTrajectory(Arrays.asList(
                 new Pose2d(0,-1 ,Rotation2d.fromDegrees(-60 /*Pending Test*/ )),
-                new Pose2d(-3,-3 ,Rotation2d.fromDegrees(-60 /*Pending Test*/ ))), forwardConfig5
+                new Pose2d(1.45,-1.30 ,Rotation2d.fromDegrees(-60 /*Pending Test*/ ))), forwardConfig5
         );
 
         TrajectoryConfig forwardConfig6 = new TrajectoryConfig(0.5, 0.2);
         forwardConfig.setReversed(false);
         Trajectory GoToObservationZone3 = TrajectoryGenerator.generateTrajectory(Arrays.asList(
-                new Pose2d(-3,-3,Rotation2d.fromDegrees(-130 /*Pending Test*/ )),
-                new Pose2d(0,-1 ,Rotation2d.fromDegrees(-130 /*Pending Test*/ ))), forwardConfig6
-        );
-
-        TrajectoryConfig forwardConfig7 = new TrajectoryConfig(0.5, 0.2);
-        forwardConfig.setReversed(false);
-        Trajectory getToStartingPosition = TrajectoryGenerator.generateTrajectory(Arrays.asList(
-                new Pose2d(0,-1,Rotation2d.fromDegrees(0 /*Pending Test*/ )),
-                new Pose2d(0,0 ,Rotation2d.fromDegrees(0 /*Pending Test*/ ))), forwardConfig7
+                new Pose2d(1.40,-1.30,Rotation2d.fromDegrees(0)),
+                new Pose2d(0,-1 ,Rotation2d.fromDegrees(0))), forwardConfig6
         );
 
         SequentialCommandGroup testCommandGroup = new SequentialCommandGroup(
-                new TurnToAngle(chassis, Rotation2d.fromDegrees(-50)),
-                new RamsetteCommand(chassis, FirstSample),
-                new GroundGrab(arm, elevator),
-                new MoveIntake(intake, 1),
-                new TurnToAngle(chassis, Rotation2d.fromDegrees(-130)),
-                new RamsetteCommand(chassis, GoToObservationZone1),
-                new MoveIntake(intake, -1),
-                new TurnToAngle(chassis, Rotation2d.fromDegrees(-55)),
-                new RamsetteCommand(chassis, SecondSample),
-                new GroundGrab(arm, elevator),
-                new MoveIntake(intake, 1),
-                new TurnToAngle(chassis, Rotation2d.fromDegrees(-130)),
-                new RamsetteCommand(chassis, GoToObservationZone2),
-                new MoveIntake(intake, -1),
-                new TurnToAngle(chassis, Rotation2d.fromDegrees(-60)),
-                new RamsetteCommand(chassis, ThirdSample),
-                new GroundGrab(arm, elevator),
-                new MoveIntake(intake, 1),
-                new TurnToAngle(chassis, Rotation2d.fromDegrees(-130)),
-                new RamsetteCommand(chassis,GoToObservationZone3),
-                new MoveIntake(intake, -1),
                 new TurnToAngle(chassis, Rotation2d.fromDegrees(0)),
-                new RamsetteCommand(chassis, getToStartingPosition)
+                new RamsetteCommand(chassis, FirstSample),
+                new TurnToAngle(chassis, Rotation2d.fromDegrees(0)),
+                new RamsetteCommand(chassis, GoToObservationZone1),
+                new TurnToAngle(chassis, Rotation2d.fromDegrees(0)),
+                new RamsetteCommand(chassis, SecondSample),
+                new TurnToAngle(chassis, Rotation2d.fromDegrees(0)),
+                new RamsetteCommand(chassis, GoToObservationZone2),
+                new TurnToAngle(chassis, Rotation2d.fromDegrees(0)),
+                new RamsetteCommand(chassis, ThirdSample),
+                new TurnToAngle(chassis, Rotation2d.fromDegrees(0)),
+                new RamsetteCommand(chassis,GoToObservationZone3)
 
         );
 
