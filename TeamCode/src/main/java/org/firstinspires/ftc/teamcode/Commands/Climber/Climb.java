277b5855c8c9848dc0b2089951733d8ea821c37c
AlariiHP <a01572813@gmail.com>
2025-01-22 20:02:37 -0600

    RetractArmElevator, hace falta un pointer para poder marcar un index cntralizado que no cree copias. El chiste es modificar el espacio de memoria que ocupa la variable. Aparte esto se hace con new, etc-

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Climber/Climb.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Climber/Climb.java
@@ -1,7 +1,12 @@
 package org.firstinspires.ftc.teamcode.Commands.Climber;
+
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+
 import org.firstinspires.ftc.teamcode.Commands.Arm.MoveArm;
 import org.firstinspires.ftc.teamcode.Commands.Elevator.ElevatorPositions;
+import org.firstinspires.ftc.teamcode.Commands.StowAll;
 import org.firstinspires.ftc.teamcode.Subsystems.Arm;
 import org.firstinspires.ftc.teamcode.Subsystems.Constants;
 import org.firstinspires.ftc.teamcode.Subsystems.Elevator;
@@ -10,8 +15,12 @@ public class Climb extends SequentialCommandGroup {
 
     public Climb (Arm arm, Elevator elevator){
         addCommands(
-            new MoveArm(arm, Constants.Arm.ARM_CLIMB),
-            new ElevatorPositions(elevator, Constants.Elevator.ELEVATOR_CLIMB)
+            new MoveArm(arm, Constants.Arm.ARM_CLIMB).withTimeout(500),
+            new ElevatorPositions(elevator,20),
+            new WaitCommand(4000),
+
+                new ElevatorPositions(elevator, 0 ).withTimeout(500),
+            new MoveArm(arm, 0).withTimeout(500)
         );
     }
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ModifiedRamsette.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Commands;
+
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Chassis;
+
+public class ModifiedRamsette extends SequentialCommandGroup {
+    public ModifiedRamsette(Chassis chassis, double distance){
+
+
+
+    }
+}
