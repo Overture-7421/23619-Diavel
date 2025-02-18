b0e99ddc7b351215013c0c279f99d82045ef5cc2
AlariiHP <a01572813@gmail.com>
2025-02-18 17:11:34 -0600

    double servo in wrist ready (copied from valkyre)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Wrist/MoveWrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/Wrist/MoveWrist.java
@@ -1,29 +1,30 @@
+
 package org.firstinspires.ftc.teamcode.Commands.Wrist;
 
-import com.arcrobotics.ftclib.command.CommandBase;
 import com.arcrobotics.ftclib.util.Timing;
-import org.firstinspires.ftc.teamcode.Subsystems.Intake;
+import com.arcrobotics.ftclib.command.CommandBase;
 import org.firstinspires.ftc.teamcode.Subsystems.Wrist;
 
 import java.util.concurrent.TimeUnit;
 
 public class MoveWrist extends CommandBase {
     private Wrist wrist;
-    private double WristMotorPosition;
+    private double wristMotorsPosition;
     private Timing.Timer timer;
 
-    public MoveWrist(Wrist subsystem, double WristMotorPosition) {
-        this.WristMotorPosition = WristMotorPosition;
-        wrist = subsystem;
+    public MoveWrist(Wrist wrist, double wristMotorsPosition) {
+        this.wristMotorsPosition = wristMotorsPosition;
+        this.wrist = wrist;
         timer = new Timing.Timer(1, TimeUnit.SECONDS);
-        addRequirements(subsystem);
+        addRequirements(wrist);
     }
 
     @Override
     public void initialize() {
-        wrist.WristPosition(WristMotorPosition);
+        wrist.wristPosition(wristMotorsPosition);
         timer.start();
     }
+
 
     @Override
     public boolean isFinished() {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Wrist.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/Wrist.java
@@ -1,23 +1,39 @@
+
 package org.firstinspires.ftc.teamcode.Subsystems;
+
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.ServoEx;
 import com.arcrobotics.ftclib.hardware.SimpleServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.arcrobotics.ftclib.hardware.ServoEx;
-
 
 public class Wrist extends SubsystemBase {
 
     // Motor Declaration
-    private ServoEx wristServo;
-
+    private final ServoEx wrist_RightServo; // Declare left claw servo
+    private final ServoEx wrist_LeftServo; //Declare right claw servo
 
     public Wrist (HardwareMap hardwareMap) {
         //Servos IDs
-        wristServo = new SimpleServo(hardwareMap, "wristServo", 0, 1.0);
+        wrist_RightServo = new SimpleServo(hardwareMap, "wrist_RightServo", 0, 2.0);
+        wrist_LeftServo = new SimpleServo(hardwareMap, "wrist_LeftServo", 0, 2.0);
+        wrist_LeftServo.setInverted(false);
+        wrist_RightServo.setInverted(true);
     }
 
-    public void WristPosition(double wrist_Position) {
-        wristServo.setPosition(wrist_Position);
+    public void wristPosition(double wristMotorsPosition) {
+        wrist_RightServo.setPosition(wristMotorsPosition);
+        wrist_LeftServo.setPosition(wristMotorsPosition);
+
+    }
+
+    public double getRightServoPosition(){
+        double right = wrist_RightServo.getAngle();
+        return (right);
+    }
+
+    public double getLeftServoPosition(){
+        double left = wrist_LeftServo.getPosition();
+        return (left);
     }
 
 }
